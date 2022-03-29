bc7a2b9df52dcc2d319517cbebe915a111b264ab
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-03-29 16:38:03 +0300

    Added msk stocks

--- app.py
+++ app.py
@@ -5,6 +5,37 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+sleep = 5
+
+def update_ticker(Link):
+
+    headers = {
+        'user agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/91.0.4472.135 Safari/537.36"}
+    html = requests.get(Link, headers)
+    soup = BeautifulSoup(html.content, 'html.parser')
+    convert = soup.findAll('div', {'class': 'YMlKec fxKbKc'})
+    price = str(convert[0].text[1:])
+    return price
+
+
+LKOH = update_ticker("https://www.google.com/finance/quote/LKOH:MCX")
+GAZP = update_ticker("https://www.google.com/finance/quote/GAZP:MCX")
+SBER = update_ticker("https://www.google.com/finance/quote/SBER:MCX")
+YNDX = update_ticker("https://www.google.com/finance/quote/YNDX:MCX")
+PHOR = update_ticker("https://www.google.com/finance/quote/PHOR:MCX")
+GMKN = update_ticker("https://www.google.com/finance/quote/GMKN:MCX")
+ROSN = update_ticker("https://www.google.com/finance/quote/ROSN:MCX")
+TCSGDR = update_ticker("https://www.google.com/finance/quote/TCSG:MCX")
+VKCODR = update_ticker("https://www.google.com/finance/quote/VKCO:MCX")
+POLY = update_ticker("https://www.google.com/finance/quote/POLY:MCX")
+AKRN = update_ticker("https://www.google.com/finance/quote/AKRN:MCX")
+NVTK = update_ticker("https://www.google.com/finance/quote/NVTK:MCX")
+AFLT = update_ticker("https://www.google.com/finance/quote/AFLT:MCX")
+VTBR = update_ticker("https://www.google.com/finance/quote/VTBR:MCX")
+MGNT = update_ticker("https://www.google.com/finance/quote/MGNT:MCX")
+
 
 @app.route('/')
 @app.route('/home')
@@ -18,7 +49,9 @@ def about():
 @app.route('/msc-market')
 def rumarket():
     dt = datetime.datetime.now()
-    return render_template("msk-market.html", time=dt.strftime('%d.%m.%Y %H:%M:%S'))
+    return render_template("msk-market.html", time=dt.strftime('%d.%m.%Y %H:%M:%S'), LKOH=LKOH, GAZP=GAZP,
+                           SBER=SBER, YNDX=YNDX, PHOR=PHOR, GMKN=GMKN, ROSN=ROSN, TCSGDR=TCSGDR, VKCODR=VKCODR,
+                           POLY=POLY, AKRN=AKRN, NVTK=NVTK, AFLT=AFLT, VTBR=VTBR, MGNT=MGNT)
 
 @app.route('/spb-market')
 def market():
