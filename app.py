68c5c89e3eae9ca153bbf0cc8b103d1a34f3dbe7
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-03-29 17:07:18 +0300

    Changed tab name

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-sleep = 5
-
 def update_ticker(Link):
 
     headers = {
@@ -27,8 +25,8 @@ YNDX = update_ticker("https://www.google.com/finance/quote/YNDX:MCX")
 PHOR = update_ticker("https://www.google.com/finance/quote/PHOR:MCX")
 GMKN = update_ticker("https://www.google.com/finance/quote/GMKN:MCX")
 ROSN = update_ticker("https://www.google.com/finance/quote/ROSN:MCX")
-TCSGDR = update_ticker("https://www.google.com/finance/quote/TCSG:MCX")
-VKCODR = update_ticker("https://www.google.com/finance/quote/VKCO:MCX")
+TCSG = update_ticker("https://www.google.com/finance/quote/TCSG:MCX")
+VKCO = update_ticker("https://www.google.com/finance/quote/VKCO:MCX")
 POLY = update_ticker("https://www.google.com/finance/quote/POLY:MCX")
 AKRN = update_ticker("https://www.google.com/finance/quote/AKRN:MCX")
 NVTK = update_ticker("https://www.google.com/finance/quote/NVTK:MCX")
@@ -50,7 +48,7 @@ def about():
 def rumarket():
     dt = datetime.datetime.now()
     return render_template("msk-market.html", time=dt.strftime('%d.%m.%Y %H:%M:%S'), LKOH=LKOH, GAZP=GAZP,
-                           SBER=SBER, YNDX=YNDX, PHOR=PHOR, GMKN=GMKN, ROSN=ROSN, TCSGDR=TCSGDR, VKCODR=VKCODR,
+                           SBER=SBER, YNDX=YNDX, PHOR=PHOR, GMKN=GMKN, ROSN=ROSN, TCSG=TCSG, VKCO=VKCO,
                            POLY=POLY, AKRN=AKRN, NVTK=NVTK, AFLT=AFLT, VTBR=VTBR, MGNT=MGNT)
 
 @app.route('/spb-market')
